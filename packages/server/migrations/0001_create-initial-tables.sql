cc16c2ded130ab638ed1a355f445b80b72b3fbd0
Ali Ansari <maliansari.coder@gmail.com>
2025-07-18 23:44:00 +0500

    changed redis to use remote redis and added postgres migrations

--- packages/server/migrations/0001_create-initial-tables.sql
+++ packages/server/migrations/0001_create-initial-tables.sql
@@ -1,6 +1,13 @@
+-- Up Migration
+
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     username VARCHAR(20) NOT NULL UNIQUE,
     passhash VARCHAR NOT NULL,
     user_id VARCHAR NOT NULL UNIQUE
 );
+
+
+-- Down Migration
+
+DROP TABLE users;
--- packages/server/utils/redis.js
+++ packages/server/utils/redis.js
@@ -1,4 +1,12 @@
 import { createClient } from "redis";
 
 
-export const redisClient = createClient()
+export const redisClient = process.env.NODE_ENV === 'production' ?
+    createClient({
+        username: process.env.REDIS_USERNAME,
+        password: process.env.REDIS_PASSWORD,
+        socket: {
+            host: process.env.REDIS_SOCKET_HOST,
+            port: process.env.REDIS_SOCKET_PORT
+        }
+    }) : createClient()
